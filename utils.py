dd9a08b8627a5c030126aa39dc735b3d10d2c007
JCGoran <goran.jelic-cizmek@unige.ch>
2022-01-14 19:10:30 +0100

    Refactored utils

--- utils.py
+++ utils.py
@@ -1,68 +1,170 @@
-class Parameter:
+"""
+Various helper utilities for Fisher objects.
+"""
+
+
+from __future__ import annotations
+
+from typing import \
+    AnyStr, \
+    Iterable
+
+import numpy as np
+
+
+
+def is_iterable(value):
     """
-    Class that contains everything to do with the parameter (its fiducial value, etc.)
+    Checks whether an object is iterable.
+    """
+    try:
+        _ = iter(value)
+        return True
+    except TypeError:
+        return False
+
+
+
+def float_to_latex(value : float):
+    """
+    Format a float into a useful LaTeX representation.
+    """
+    float_str = f"{value:.2g}"
+    if "e" in float_str:
+        base, exponent = float_str.split("e")
+        return f"{base} \\times 10^{{{int(exponent)}}}"
+
+    return float_str
+
+
+
+class ParameterNotFoundError(Exception):
+    """
+    Error raised when a parameter is not found in the Fisher object.
     """
     def __init__(
         self,
-        value : float = 0,
-        # TODO other data
+        name : AnyStr,
+        names : Iterable[AnyStr],
     ):
-        self._fiducial = value
-
-    @property
-    def fiducial(self):
-        return self._fiducial
-
-    @fiducial.setter
-    def fiducial(self, value):
-        self._fiducial = value
-
-    def __repr__(self):
-        return f'fiducial: {self.fiducial}'
+        self.message = f'Parameter \'{name}\' not found in array {names}'
+        super().__init__(self.message)
 
 
-class CustomSet:
+
+class MismatchingSizeError(Exception):
     """
-    A custom set-like class which preserves ordering of elements.
-    Internally, it's a Python `dict` with all values mapping to `None`.
+    Error for handling a list of arrays that have mismatching sizes.
+    """
+    def __init__(
+        self,
+        *args,
+    ):
+        sizes = [len(arg) for arg in args]
+        self.message = f'Mismatching lengths: {", ".join(sizes)}'
+        super().__init__(self.message)
+
+
+
+class HTMLWrapper:
+    """
+    A wrapper class for pretty printing objects in a Jupyter notebook.
+    """
+    def __init__(self, string):
+        self._string = string
+    def _repr_html_(self):
+        return self._string
+
+
+
+def make_html_table(
+    values : Iterable,
+    names : Iterable = None,
+    fmt_values : str = '{}',
+    fmt_names : str = '{}',
+    title : str = '',
+):
+    """
+    Makes a HTML formatted table with names (optional) and values.
+
+    Parameters
+    ----------
+    values : Iterable
+        the values in the table
+
+    names : Iterable, default = None
+        the names of the values in the table. Must have the same length as `values`
+
+    fmt_values : str, default = '{}'
+        the format string for the values
+
+    fmt_names : str, default = '{}'
+        the format string for the names
+
+    title : str, default = ''
+        the title of the table
     """
 
-    def __init__(self, data):
-        self._data = {key : Parameter() for key in data}
+    if title:
+        title = f'<tr><th align="left">{title}</th></tr>'
 
-    @property
-    def data(self):
-        return tuple(self._data.keys())
+    temp = (
+        '<tr>' + (f'<th>{fmt_names}</th>' * len(names)).format(*names) + '</tr>'
+    ) if names else ''
+    header = f'<thead>{title}{temp}</thead>'
 
-    def index(self, idx : int):
-        return list(self._data.keys()).index(idx)
+    body = '<tbody>' + (
+        f'<td>{fmt_values}</td>' * len(values)
+    ).format(*values) + '</tbody>'
 
-    def __getitem__(self, key):
-        if not isinstance(key, int) and key not in self._data.keys():
-            raise KeyError(f'Key {key} not found')
-        if isinstance(key, int):
-            return list(self._data.keys())[key]
-        return self._data[key]
+    return f'<table>{header}{body}</table>'
 
-    def __setitem__(self, key, value):
-        if key not in self._data.keys():
-            raise KeyError(f'Key {key} not found')
-        if value != key and value in self._data.keys():
-            raise ValueError(f'The value {value} already exists')
-        if value != key:
-            self._data = {_ if _ != key else value : __ for _, __ in self._data.items()}
 
-    def __iter__(self):
-        return self._data.__iter__()
 
-    def __repr__(self):
-        return str(tuple(self._data.keys()))
+def make_default_names(
+    size : int,
+    character : str = 'p',
+):
+    """
+    Returns the array with default names `p1, ..., pn`
+    """
+    if size < 0:
+        raise ValueError
+    return np.array([f'{character}{_ + 1}' for _ in range(size)])
 
-    def __len__(self):
-        return len(self._data.keys())
 
-    def __eq__(self, other):
-        return tuple(self._data.keys()) == other
 
-    def __hash__(self):
-        return hash(self.data)
+def is_square(values):
+    """
+    Checks whether a numpy array-able object is square.
+    """
+
+    shape = np.shape(values)
+    return all(shape[0] == _ for _ in shape)
+
+
+
+def is_symmetric(values):
+    """
+    Checks whether a numpy array-able object is symmetric.
+    """
+    return np.allclose(
+        np.transpose(values),
+        values
+    )
+
+
+
+def is_positive_semidefinite(values):
+    """
+    Checks whether a numpy array-able object is positive semi-definite.
+    """
+    return np.all(np.linalg.eigvalsh(values) >= 0)
+
+
+
+def has_positive_diagonal(values):
+    """
+    Checks whether all of the values on the diagonal are positive.
+    """
+    return np.all(np.diag(values) >= 0)

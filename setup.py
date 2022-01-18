292cd4795a9e50c0f05d154f9a9e131431dc7308
JCGoran <goran.jelic-cizmek@unige.ch>
2022-01-18 20:24:08 +0100

    Added setup scripts

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='fitk',
+    version='1.0.0',
+    description='The Fisher Information ToolKit',
+    author='Goran Jelic-Cizmek',
+    author_email='goran.jelic-cizmek@unige.ch',
+    install_requires=[
+        'matplotlib>=3.4',
+        'numpy>=1.16.0',
+        'scipy>=1.2.0',
+    ],
+    packages=['fitk'],
+)

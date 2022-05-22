b13b403bfed9221f1ec307f4bbef26eec198e228
JCGoran <goran.jelic-cizmek@unige.ch>
2022-05-22 18:36:25 +0200

    Reformatted setup file with black

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='fitk',
-    version='1.0.0',
-    description='The Fisher Information ToolKit',
-    author='Goran Jelic-Cizmek',
-    author_email='goran.jelic-cizmek@unige.ch',
+    name="fitk",
+    version="1.0.0",
+    description="The Fisher Information ToolKit",
+    author="Goran Jelic-Cizmek",
+    author_email="goran.jelic-cizmek@unige.ch",
     install_requires=[
-        'matplotlib>=3.4',
-        'numpy>=1.16.0',
-        'scipy>=1.2.0',
+        "matplotlib>=3.3",
+        "numpy>=1.16.0",
+        "scipy>=1.2.0",
     ],
-    packages=['fitk'],
+    packages=["fitk"],
 )

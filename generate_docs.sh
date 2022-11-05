d33fd73ed7cae8395f1b97af7945c6a43b26c302
JCGoran <goran.jelic-cizmek@unige.ch>
2022-11-06 00:44:35 +0100

    Fixed displaying of version in docs
    Poetry was not working in the build script, so it is simpler to just use
    `cat [VERSION_FILE]` to read it.

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,8 +3,8 @@
 # need to substitute the version in the init file
 init_path="fitk/__init__.py"
 init_content="$(cat "${init_path}")"
-package_version="$(python3 -m poetry version --short)"
+package_version="$(cat "fitk/VERSION.txt")"
 sed -i 's/\$VERSION/'"${package_version}"'/g' "${init_path}"
 python3 -m pdoc --docformat numpy --math -o docs/ fitk
 # restore the init file
-printf '%s' "${init_content}" > "${init_path}"
+printf '%s\n' "${init_content}" > "${init_path}"

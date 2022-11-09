d0af871a5f2c65dc4bd12923839fa6709a9edd16
JCGoran <goran.jelic-cizmek@unige.ch>
2022-11-09 10:56:46 +0100

    Updated docstring for interfaces

--- fitk/interfaces/__init__.py
+++ fitk/interfaces/__init__.py
@@ -3,17 +3,17 @@ This module defines interfaces to various third-party software with which
 one can compute Fisher matrices using finite differences.
 
 ### Important notice
-Due to the complexities involved in distributing software that is not
-under direct control of the developers/maintainers of `fitk` (version,
-installation, license issues, etc.), any third-party software (such as
-cosmological codes) is *not* bundled with `fitk` (i.e. installed
-automatically), and must be installed separately by the user.
+Due to the complexities involved in distributing software that is not under
+direct control of the developers/maintainers of `fitk` (version, installation,
+license issues, etc.), any external, third-party software (such as cosmological
+codes) is *not* bundled with `fitk` (i.e. installed automatically), and must be
+installed separately by the user.
 
 ### Computation of custom derivatives
-To define a new interface, one should define a class that inherits from
-`fitk.fisher_derivative.FisherDerivative`, and implements either the `signal`
-or the `covariance` method; below outlines the steps to create an interface of
-your own using a minimal amount of code:
+To define a new interface for computing derivatives, one should define a class
+that inherits from `fitk.fisher_derivative.FisherDerivative`, and implements
+either the `signal` or the `covariance` methods (or both); below outlines the
+steps to create an interface of your own using a minimal amount of code:
 
 ```python
 from __future__ import annotations # required for Python 3.7
@@ -57,12 +57,16 @@ class MyFisher(FisherDerivative):
         return np.array([...])
 ```
 
-It is recommended (but not required) that any additional parameters should be
-stored in the constructor (i.e. `__init__` method), to prevent forgetting to
-pass them when computing the derivatives.
-Additionally, any extra information (members and methods) should be documented
-accordingly.
-Finally, if the external module could not be loaded, importing the module
-should raise an `ImportError` if the user attempts to instantiate a class
-with that module missing.
+The following are some general recommendations when creating a new interface:
+- for ease of use, any additional parameters specifying the configuration for
+  the interface should be passed to the constructor (i.e. the `__init__`
+  method)
+- extra information (methods, members, custom parameters) should be documented
+  accordingly
+- if the external module could not be imported, rather than directly raising an
+  exception, it is preferable that instantiating a class from that module
+  raises an `ImportError` instead. A simple way to accomplish this is to wrap
+  the import in a `try...except ImportError`, pass the success result (perhaps
+  stored as a boolean) to the constructor of the class, and then only raise the
+  `ImportError` there
 """

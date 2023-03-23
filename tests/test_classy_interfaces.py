dd0c4f2220d9a59b74d9407f69b53032a37eb12b
JCGoran <goran.jelic-cizmek@unige.ch>
2023-03-23 19:10:28 +0100

    Added more tests for interface to CLASS code

--- tests/test_classy_interfaces.py
+++ tests/test_classy_interfaces.py
@@ -23,29 +23,45 @@ DATADIR_INPUT = Path(__file__).resolve().parent / "data_input"
 def helper(cosmo: FisherDerivative):
     signal = cosmo.signal()
     cov = cosmo.covariance()
+    assert cov.ndim == 2
+    assert np.all(np.diag(cov) > 0)
+    assert np.all(np.linalg.eigvalsh(cov) >= 0)
+    assert signal.shape[0] == cov.shape[0] == cov.shape[1]
     assert np.allclose(cov.T, cov)
     return np.linalg.inv(cov) @ signal
 
 
-def test_outputs():
-    cosmo_all = ClassyCMBDerivative(config={"output": "tCl,pCl"})
-    helper(cosmo_all)
+class TestClassy:
+    @pytest.mark.parametrize("output", ["tCl,pCl", "tCl", "pCl"])
+    def test_outputs(self, output):
+        cosmo = ClassyCMBDerivative(config={"output": output})
+        helper(cosmo)
 
-    cosmo_t = ClassyCMBDerivative(config={"output": "tCl"})
-    helper(cosmo_t)
+    @pytest.mark.parametrize("lmax", [10, 100, 999])
+    def test_lmax(self, lmax: int):
+        cosmo = ClassyCMBDerivative(config={"output": "tCl", "l_max_scalars": lmax})
 
-    cosmo_p = ClassyCMBDerivative(config={"output": "pCl"})
-    helper(cosmo_p)
+        assert cosmo.signal().shape == (lmax - 1,)
+        helper(cosmo)
 
+    def test_from_file(self):
+        cosmo = ClassyCMBDerivative.from_file(
+            DATADIR_INPUT / "classy_parameter_file.ini"
+        )
+        helper(cosmo)
 
-def test_another():
-    lmax = 999
-    cosmo_lmax = ClassyCMBDerivative(config={"output": "tCl", "l_max_scalars": lmax})
-
-    assert cosmo_lmax.signal().shape == (lmax - 1,)
-
-
-def test_from_file():
-    cosmo = ClassyCMBDerivative.from_file(DATADIR_INPUT / "classy_parameter_file.ini")
-    cosmo.signal()
-    cosmo.covariance()
+    @pytest.mark.parametrize(
+        "output,benchmark",
+        [
+            (["tCl", "pCl"], {"temperature": True, "polarization": True}),
+            ("tCl,pCl", {"temperature": True, "polarization": True}),
+            ("tCl", {"temperature": True, "polarization": False}),
+            ("pCl", {"temperature": False, "polarization": True}),
+            (["tCl"], {"temperature": True, "polarization": False}),
+            (["pCl"], {"temperature": False, "polarization": True}),
+        ],
+    )
+    def test_parse_outputs(self, output, benchmark):
+        cosmo = ClassyCMBDerivative(config={"output": output})
+        result = cosmo._parse_outputs()
+        assert result == benchmark

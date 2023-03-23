75717e4a60ca99c0ca284b7b68170fea74032c2d
JCGoran <goran.jelic-cizmek@unige.ch>
2023-03-23 13:16:10 +0100

    Modified testing scripts

--- check_coverage.sh
+++ check_coverage.sh
@@ -5,11 +5,12 @@
 set -eux
 
 check_coverage(){
+    excluded='not __init__.py and not interfaces'
     if [ "${1:-}" = '-i' ] || [ "${1:-}" = '--images' ]
     then
-        poetry run pytest --mpl --doctest-modules --mpl-generate-summary=html -k 'not test_interfaces and not __init__.py and not interfaces' --cov=fitk/ --cov=tests/ --cov-report=xml tests/ fitk/
+        poetry run pytest --mpl --doctest-modules --mpl-generate-summary=html -k "${excluded}" --cov=fitk/ --cov=tests/ --cov-report=xml tests/ fitk/
     else
-        poetry run pytest --doctest-modules -k 'not test_interfaces and not __init__.py and not interfaces' --cov=fitk/ --cov=tests/ --cov-report=xml tests/ fitk/
+        poetry run pytest --doctest-modules -k "${excluded}" --cov=fitk/ --cov=tests/ --cov-report=xml tests/ fitk/
     fi
     poetry run coverage html
     poetry run coverage report

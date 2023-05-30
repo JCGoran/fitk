719402bb7cc937f0cbcd6867aea8e045977584be
JCGoran <goran.jelic-cizmek@unige.ch>
2023-05-30 15:02:26 +0200

    Updated tests

--- tests/test_camb_interfaces.py
+++ tests/test_camb_interfaces.py
@@ -9,7 +9,7 @@ import numpy as np
 import pytest
 from helpers import get_signal_and_covariance, validate_signal_and_covariance
 
-from fitk import D, P
+from fitk import D, FisherFigure2D, P
 from fitk.interfaces.camb_interfaces import CambCMBDerivative
 from fitk.interfaces.classy_interfaces import ClassyCMBDerivative
 
@@ -18,6 +18,7 @@ DATADIR_INPUT = Path(__file__).resolve().parent / "data_input"
 
 class TestCMB:
     def test_signal(self):
+        lmax = 100
         cosmo = CambCMBDerivative(
             config={
                 "omch2": 0.121203,
@@ -27,6 +28,7 @@ class TestCMB:
                 "omk": 0,
                 "As": 2e-9,
                 "nnu": 3.046,
+                "max_l": lmax,
             }
         )
         signal = cosmo.signal()
@@ -41,11 +43,13 @@ class TestCMB:
                 "A_s": 2e-9,
                 "N_ur": 3.046,
                 "output": "tCl,pCl",
+                "l_max_scalars": lmax,
             }
         )
         s = c.signal()
 
-        r = slice(2500, 4000)
+        # r = slice(2500, 4000)
+        r = slice(0, lmax)
 
         s = s[r]
         signal = signal[r]
@@ -60,3 +64,39 @@ class TestCMB:
         ax[0].legend()
         ax[1].plot(100 * (1 - signal / s))
         fig.savefig("camb_vs_class.pdf", dpi=300, bbox_inches="tight")
+
+    @pytest.mark.parametrize("lmax", [100, 999])
+    def test_lmax(self, lmax: int):
+        cosmo = CambCMBDerivative(
+            config={
+                "omch2": 0.121203,
+                "H0": 67,
+                "max_l": lmax,
+            }
+        )
+        signal, cov = get_signal_and_covariance(cosmo)
+
+        # the 3 is here because CAMB outputs TT, EE, and TE by default
+        assert signal.shape == (3 * (lmax - 1),)
+        validate_signal_and_covariance(signal, cov)
+
+    def test_fisher_matrix(self):
+        parameters = [
+            D(P("omch2", 0.27 * 0.67**2, latex_name=r"$\omega_\mathrm{cdm}$"), 1e-3),
+            D(P("ombh2", 0.04 * 0.67**2, latex_name=r"$\omega_\mathrm{b}$"), 1e-3),
+            D(P("H0", 60, latex_name=r"$H_0$"), 1e-3),
+        ]
+        cosmo = CambCMBDerivative(
+            config={
+                "max_l": 100,
+                **{_.parameter.name: _.parameter.fiducial for _ in parameters},
+            }
+        )
+        fm = cosmo.fisher_matrix(*parameters)
+
+        assert fm.is_valid()
+
+        ff = FisherFigure2D(show_joint_dist=True, show_1d_curves=True)
+
+        ff.plot(fm)
+        ff.savefig("camb_forecast.pdf")

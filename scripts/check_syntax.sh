a31f17dd9a03a0f6151f4fc23a8367704586560e
JCGoran <goran.jelic-cizmek@unige.ch>
2024-03-13 19:02:17 +0100

    Move all scripts to `scripts`

--- scripts/check_syntax.sh
+++ scripts/check_syntax.sh
@@ -3,12 +3,12 @@
 set -eux
 
 validate(){
-    modules='fitk/ tests/'
-    poetry run black --check ${modules}
+    modules='python/fitk/ tests/'
+    python -m black --check ${modules}
     # the E0611 is because Pylint detects some native modules (like
     # `collections.abc.Collection`) as missing on some machines for one reason
     # or another
-    poetry run pylint --disable=W,R,C,E0611 ${modules}
+    python -m pylint --disable=W,R,C,E0611 ${modules}
 }
 
 validate
